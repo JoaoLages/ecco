042ac5e727bcc0ae1890a360ed42e6424aac681f
JoaoLages <joaop.glages@gmail.com>
2022-03-16 16:29:23 +0000

    update ecco

--- src/ecco/lm.py
+++ src/ecco/lm.py
@@ -375,7 +375,7 @@ class LM(object):
         #     token = self.tokenizer.decode([i])
         #     tokens.append(token)
 
-        attributions = [x[:len(n_input_tokens)] for x in self.attributions]
+        attributions = [x[:n_input_tokens] for x in self.attributions]
         attn = getattr(output, "attentions", None)
 
         return OutputSeq(**{'tokenizer': self.tokenizer,
