cda0abcc19ee5149c74ece2c08fb73024c2d6901
JoaoLages <joaop.glages@gmail.com>
2022-03-17 18:26:52 +0000

    fix autoregressive behaviour

--- src/ecco/lm.py
+++ src/ecco/lm.py
@@ -381,7 +381,8 @@ class LM(object):
         #     token = self.tokenizer.decode([i])
         #     tokens.append(token)
 
-        attributions = {k: [x[:n_input_tokens] for x in v] for k, v in self.attributions.items()}
+        # attributions = {k: [x[:n_input_tokens] for x in v] for k, v in self.attributions.items()}
+        attributions = self.attributions
         attn = getattr(output, "attentions", None)
 
         return OutputSeq(**{'tokenizer': self.tokenizer,
